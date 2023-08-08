0e475036ba3f5cef7faf274157c7546c76add867
DilankaSandeep <dilankacm@gmail.com>
2023-08-08 08:16:11 +0530

    Updated to check the Subject name reentering

--- StatementspracticeAssignment.java
+++ StatementspracticeAssignment.java
@@ -7,15 +7,15 @@ public class StatementspracticeAssignment {
         final String COLOR_RED = "\033[31;1m";
         final String RESET = "\033[0m";
         final String COLOR_BLUE = "\033[34;1m ";
-        final String COLOR_GREEN= "\033[32;1m ";
-        final String COLOR_YELLOW="\033[33;1m ";
+        final String COLOR_GREEN = "\033[32;1m ";
+        final String COLOR_YELLOW = "\033[33;1m ";
 
         System.out.print("Enter your Name: ");
         String name = scanner.nextLine();
         if (name.strip().length() == 0) {
             System.out.printf("%sInvalid name Input%s \n", COLOR_RED, RESET);
             return;
-           
+
         } else {
             name = name.toUpperCase();
             System.out.print("Enter your Age: ");
@@ -27,7 +27,7 @@ public class StatementspracticeAssignment {
             } else {
                 System.out.print("Enter your Subject1: ");
                 String subject1 = scanner.nextLine();
-                if (!((subject1.startsWith("SE-")| subject1.startsWith("se-")) & subject1.strip().length()>3)) {
+                if (!((subject1.startsWith("SE-") | subject1.startsWith("se-")) & subject1.strip().length() > 3)) {
                     System.out.printf("%sInvalid Subject Input%s \n", COLOR_RED, RESET);
                     System.exit(0);
                 } else {
@@ -40,83 +40,98 @@ public class StatementspracticeAssignment {
                     String status1;
                     if (marks1 >= 75) {
                         status1 = COLOR_YELLOW + "Distint Pass " + "\033[0m";
-                    } else if (marks1>=65){
+                    } else if (marks1 >= 65) {
                         status1 = COLOR_GREEN + "Credit Pass  " + "\033[0m";
-                    }else if (marks1 >= 55) {
+                    } else if (marks1 >= 55) {
                         status1 = COLOR_BLUE + "Pass         " + RESET;
                     } else {
                         status1 = COLOR_RED + " Fail         " + RESET;
                     }
                     System.out.print("Enter your Subject2: ");
                     String subject2 = scanner.nextLine();
-                    if (!((subject2.startsWith("SE-")| subject2.startsWith("se-")) & subject2.strip().length()>3)) {
-                        System.out.printf("%sInvalid Subject Input%s \n", COLOR_RED, RESET);
+                    if (subject1.equalsIgnoreCase(subject2)) {
+                        System.out.printf("%sSubject is already entered%s \n", COLOR_RED, RESET);
                         System.exit(0);
                     } else {
-                        subject2 = subject2.strip();
-                        String subject2Number = subject2.substring(3);
-                        int sub2Number = Integer.parseInt(subject2Number);
-                        System.out.print("Enter your Marks2: ");
-                        double marks2 = scanner.nextDouble();
-                        scanner.nextLine();
-                        String status2;
-                        if (marks2 >= 75) {
-                            status2 = COLOR_YELLOW + "Distint Pass " + "\033[0m";
-                        } else if (marks2>=65){
-                            status2 = COLOR_GREEN + "Credit Pass  " + RESET;
-                        }else if (marks2 >= 55) {
-                            status2 = COLOR_BLUE + "Pass         " + RESET;
-                        } else {
-                            status2 = COLOR_RED + " Fail         " + RESET;
-                        }
-                        System.out.print("Enter your Subject3: ");
-                        String subject3 = scanner.nextLine();
-                        if (!((subject3.startsWith("SE-")| subject3.startsWith("se-")) & subject3.strip().length()>3)) {
+                        if (!((subject2.startsWith("SE-") | subject2.startsWith("se-"))
+                                & subject2.strip().length() > 3)) {
                             System.out.printf("%sInvalid Subject Input%s \n", COLOR_RED, RESET);
                             System.exit(0);
                         } else {
-                            subject3 = subject3.strip();
-                            String subject3Number = subject3.substring(3);
-                            int sub3Number = Integer.parseInt(subject3Number);
-                            System.out.print("Enter your Marks3: ");
-                            double marks3 = scanner.nextDouble();
+                            subject2 = subject2.strip();
+                            String subject2Number = subject2.substring(3);
+                            int sub2Number = Integer.parseInt(subject2Number);
+                            System.out.print("Enter your Marks2: ");
+                            double marks2 = scanner.nextDouble();
                             scanner.nextLine();
-                            String status3;
-                            if (marks3 >= 75) {
-                                status3 = COLOR_YELLOW + "Distint Pass " + "\033[0m";
-                            } else if (marks3>=65){
-                                status3 = COLOR_GREEN + "Credit Pass  " + RESET;
-                            } else if (marks3 >= 55) {
-                                status3 = COLOR_BLUE + "Pass         " + RESET;
+                            String status2;
+                            if (marks2 >= 75) {
+                                status2 = COLOR_YELLOW + "Distint Pass " + "\033[0m";
+                            } else if (marks2 >= 65) {
+                                status2 = COLOR_GREEN + "Credit Pass  " + RESET;
+                            } else if (marks2 >= 55) {
+                                status2 = COLOR_BLUE + "Pass         " + RESET;
                             } else {
-                                status3 = COLOR_RED + " Fail         " + RESET;
+                                status2 = COLOR_RED + " Fail         " + RESET;
                             }
-                            double total =marks1+marks2+marks3;
-                            double avg =(total)/3;
-                            String status;
-                            if (avg >= 75) {
-                                status = COLOR_YELLOW + "Distint Pass" + "\033[0m";
-                            } else if (avg>=65){
-                                status = COLOR_GREEN + "Credit Pass" + RESET;
-                            }else if (avg >= 55) {
-                                status = COLOR_BLUE + "Pass" + RESET;
+                            System.out.print("Enter your Subject3: ");
+                            String subject3 = scanner.nextLine();
+                            if (subject3.equalsIgnoreCase(subject1) | subject3.equalsIgnoreCase(subject2)) {
+                                System.out.printf("%sSubject is already entered%s \n", COLOR_RED, RESET);
+                                System.exit(0);
                             } else {
-                                status= COLOR_RED + " Fail" + RESET;
+                                if (!((subject3.startsWith("SE-") | subject3.startsWith("se-"))
+                                        & subject3.strip().length() > 3)) {
+                                    System.out.printf("%sInvalid Subject Input%s \n", COLOR_RED, RESET);
+                                    System.exit(0);
+                                } else {
+                                    subject3 = subject3.strip();
+                                    String subject3Number = subject3.substring(3);
+                                    int sub3Number = Integer.parseInt(subject3Number);
+                                    System.out.print("Enter your Marks3: ");
+                                    double marks3 = scanner.nextDouble();
+                                    scanner.nextLine();
+                                    String status3;
+                                    if (marks3 >= 75) {
+                                        status3 = COLOR_YELLOW + "Distint Pass " + "\033[0m";
+                                    } else if (marks3 >= 65) {
+                                        status3 = COLOR_GREEN + "Credit Pass  " + RESET;
+                                    } else if (marks3 >= 55) {
+                                        status3 = COLOR_BLUE + "Pass         " + RESET;
+                                    } else {
+                                        status3 = COLOR_RED + " Fail         " + RESET;
+                                    }
+                                    double total = marks1 + marks2 + marks3;
+                                    double avg = (total) / 3;
+                                    String status;
+                                    if (avg >= 75) {
+                                        status = COLOR_YELLOW + "Distint Pass" + "\033[0m";
+                                    } else if (avg >= 65) {
+                                        status = COLOR_GREEN + "Credit Pass" + RESET;
+                                    } else if (avg >= 55) {
+                                        status = COLOR_BLUE + "Pass" + RESET;
+                                    } else {
+                                        status = COLOR_RED + " Fail" + RESET;
+                                    }
+                                    System.out.printf("+%40s+\n", "-".repeat(40));
+                                    System.out.printf("|Name   : %s%-30s%s|\n", COLOR_BLUE, name, RESET);
+                                    System.out.printf("|Age    : %-31s|\n", age + " Years old");
+                                    System.out.printf("|Status : %-42s|\n", status);
+                                    System.out.printf("|Total  : %-15.2fAvg  : %-9.2f|\n", total, avg);
+                                    System.out.printf("+%40s+\n", "-".repeat(40));
+                                    System.out.printf("|%-14s|%-10s|%-14s|\n", "Subject", "Marks", "Status");
+                                    System.out.printf("+%40s+\n", "-".repeat(40));
+                                    System.out.printf("|SE-%03d%8s|%-10.2f|%-14s|\n", sub1Number, "", marks1, status1);
+                                    System.out.printf("|SE-%03d%8s|%-10.2f|%-14s|\n", sub2Number, "", marks2, status2);
+                                    System.out.printf("|SE-%03d%8s|%-10.2f|%-14s|\n", sub3Number, "", marks3, status3);
+                                    System.out.printf("+%40s+\n", "-".repeat(40));
+                                }
                             }
-                            System.out.printf("+%40s+\n", "-".repeat(40));
-                            System.out.printf("|Name   : %s%-30s%s|\n", COLOR_BLUE, name, RESET);
-                            System.out.printf("|Age    : %-31s|\n", age + " Years old");
-                            System.out.printf("|Status : %-42s|\n", status);
-                            System.out.printf("|Total  : %-15.2fAvg  :%-10.2f|\n",total,avg);
-                            System.out.printf("+%40s+\n", "-".repeat(40));
-                            System.out.printf("|%-14s|%-10s|%-14s|\n","Subject","Marks","Status");
-                            System.out.printf("+%40s+\n", "-".repeat(40));
-                            System.out.printf("|SE-%03d%8s|%-10.2f|%-14s|\n",sub1Number,"",marks1,status1);
-                            System.out.printf("|SE-%03d%8s|%-10.2f|%-14s|\n",sub2Number,"",marks2,status2);
-                            System.out.printf("|SE-%03d%8s|%-10.2f|%-14s|\n",sub3Number,"",marks3,status3);
-                            System.out.printf("+%40s+\n", "-".repeat(40));
+
                         }
+
                     }
+
                 }
             }
         }
